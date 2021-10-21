d2a094fbef0286810d554fe0a37e08b60a1ff2c8
Mariadssantos <madudallas16@gmail.com>
2021-10-21 19:28:42 -0300

    LoginBox e MessagesBox

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,49 @@
+import styles from './styles.module.scss';
 
-export function MessageList(){
+import LogoImg from '../../assets/logo.svg';
+
+export function MessageList() {
     return(
-        <h1>MessageList</h1>
+     <div className={styles.messageListWrapper}> 
+
+     <img src={LogoImg} alt="Logo-DoWhile"  />
+
+     <ul className={styles.messageList}>
+         <li className={styles.message}>
+            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+            <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
+
+                </div>
+
+                <span>Maria Eduarda</span>
+            </div>
+         </li>
+         <li className={styles.message}>
+            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+            <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
+
+                </div>
+
+                <span>Maria Eduarda</span>
+            </div>
+         </li>
+         <li className={styles.message}>
+            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+            <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
+
+                </div>
+
+                <span>Maria Eduarda</span>
+            </div>
+         </li>
+     </ul>
+     </div>
     );
     
 }
