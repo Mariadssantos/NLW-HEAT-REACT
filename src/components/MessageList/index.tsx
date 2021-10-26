2347c89518a02a36a6d13c568badd0f615304bc3
Mariadssantos <madudallas16@gmail.com>
2021-10-26 15:50:12 -0300

    Add Last3 Messages na page home

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,50 @@
-import styles from './styles.module.scss';
+import { api } from '../../services/api';
 
+import styles from './styles.module.scss';
 import LogoImg from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
 
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data);
+        })
+    }, [])
     return(
      <div className={styles.messageListWrapper}> 
 
      <img src={LogoImg} alt="Logo-DoWhile"  />
 
      <ul className={styles.messageList}>
-         <li className={styles.message}>
-            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+         {messages.map(message => {
+
+            return (
+         <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>{message.text}</p>
             <div className={styles.messageUser}>
                 <div className={styles.userImage}>
-                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
+                <img src={message.user.avatar_url} alt={message.user.name} />
 
                 </div>
 
-                <span>Maria Eduarda</span>
+                <span>{message.user.name}</span>
             </div>
          </li>
-         <li className={styles.message}>
-            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-            <div className={styles.messageUser}>
-                <div className={styles.userImage}>
-                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
-
-                </div>
-
-                <span>Maria Eduarda</span>
-            </div>
-         </li>
-         <li className={styles.message}>
-            <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-            <div className={styles.messageUser}>
-                <div className={styles.userImage}>
-                <img src="https:github.com/Mariadssantos.png" alt="Maria Eduarda" />
-
-                </div>
-
-                <span>Maria Eduarda</span>
-            </div>
-         </li>
+         );
+         })}
      </ul>
      </div>
-    );
-    
+    )  
 }
